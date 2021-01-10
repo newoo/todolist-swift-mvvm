c58528855eea87f93766c03a5061b331e64a4b06
newoo <newoo4297@naver.com>
2021-01-11 00:18:46 +0900

    Add Todo data model structure

---
+++ todolist-swift-mvvm/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  todolist-swift-mvvm
+//
+//  Created by law on 2021/01/11.
+//  Copyright © 2021 newoo. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    var id: Int
+    let title: String
+}
