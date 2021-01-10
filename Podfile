a5ce4e529279ccd79036712c09f448af4428871d
newoo <newoo4297@naver.com>
2021-01-10 23:06:10 +0900

    First commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '13.0'
+
+target 'todolist-swift-mvvm' do
+  use_frameworks!
+
+  # Pods for todolist-swift-mvvm
+  pod 'SnapKit'
+  pod 'RxSwift', '~> 5.0.1'
+  pod 'RxCocoa', '~> 5.0.1'
+
+  target 'todolist-swift-mvvmTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Quick'
+    pod 'Nimble'
+  end
+
+end
---
+++ todolist-swift-mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todolist-swift-mvvm
+//
+//  Created by law on 2021/01/10.
+//  Copyright © 2021 newoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
