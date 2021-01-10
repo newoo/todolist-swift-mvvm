77a00f1df8ec181f2d8e4cf414c36884ded4768d
newoo <newoo4297@naver.com>
2021-01-10 23:17:40 +0900

    Add Test about TodoListViewController
    TodoListViewController when be loaded has todolist by UITableView

--- todolist-swift-mvvmTests/TodoListViewControllerSpec.swift
+++ todolist-swift-mvvmTests/TodoListViewControllerSpec.swift
@@ -10,8 +10,27 @@ import Quick
 import Nimble
 @testable import todolist_swift_mvvm
 
-class TodoListViewControllerSpec: XCTestCase {
+class TodoListViewControllerSpec: QuickSpec {
     override func spec() {
-        
+        describe("TodoListViewController") {
+            var todolistViewController: TodoListViewController!
+            
+            beforeEach {
+                todolistViewController = TodoListViewController()
+                let window = UIWindow(frame: UIScreen.main.bounds)
+                window.makeKeyAndVisible()
+                window.rootViewController = todolistViewController
+                
+                todolistViewController.beginAppearanceTransition(true, animated: false)
+                todolistViewController.endAppearanceTransition()
+            }
+            
+            context("when be loaded") {
+                it("has todolist by UITableView") {
+                    let hasTableView = todolistViewController.view.subviews.contains(where: { $0 is UITableView })
+                    expect(hasTableView).to(beTrue())
+                }
+            }
+        }
     }
 }
