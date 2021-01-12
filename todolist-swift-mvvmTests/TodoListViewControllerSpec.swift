1742165ca46f1945372c031d20db350099114c6e
newoo <newoo4297@naver.com>
2021-01-12 21:19:24 +0900

    Add and pass test TodoListViewController with new todo show new todo

--- todolist-swift-mvvmTests/TodoListViewControllerSpec.swift
+++ todolist-swift-mvvmTests/TodoListViewControllerSpec.swift
@@ -15,6 +15,7 @@ class TodoListViewControllerSpec: QuickSpec {
         describe("TodoListViewController") {
             struct Constant {
                 static let todoList = [Todo(id: 1, title: "TODO"), Todo(id: 2, title: "TODO2")]
+                static let newTodo = Todo(id: 3, title: "TODO3")
             }
             
             var todolistViewController: TodoListViewController!
@@ -57,6 +58,25 @@ class TodoListViewControllerSpec: QuickSpec {
 
                     let todoTitle = todoTableViewCell.todoTitleLabel.text
                     expect(todoTitle).toEventually(equal(Constant.todoList.first?.title), timeout: .seconds(3))
+                }
+                
+                context("with new todo") {
+                    it("show new todo with old todos") {
+                        var todoList = Constant.todoList
+                        todoList.append(Constant.newTodo)
+                        todolistViewController.viewModel.todosOutput.onNext(todoList)
+                        
+                        let todosCount = todolistViewController.tableView.visibleCells.count
+                        expect(todosCount).toEventually(equal(3), timeout: .seconds(3))
+                        
+                        guard let todoTableViewCell = todolistViewController.tableView.cellForRow(at: IndexPath(row: 2, section: 0)) as? TodoTableViewCell else {
+                            fail()
+                            return
+                        }
+
+                        let todoTitle = todoTableViewCell.todoTitleLabel.text
+                        expect(todoTitle).toEventually(equal(Constant.newTodo.title), timeout: .seconds(3))
+                    }
                 }
             }
         }
